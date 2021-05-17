9b208987d8e9110ef652b5c9861903ef6a49386f
GuiCartaxo <guilherme.cartaxo@hotmail.com>
2021-05-16 22:02:09 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/guilhermecartaxo/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.guilhermecartaxo.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.guilhermecartaxo.workshopmongo.domain.User;
+import com.guilhermecartaxo.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+	
+}
